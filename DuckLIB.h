0afbfc8bea58b5ac1457d4cc78eb4f8dda6b9837
Luna-Doggo <113269653+Luna-Doggo@users.noreply.github.com>
2023-08-13 16:29:42 -0700

    Add files via upload

---
+++ DuckLIB.h
@@ -0,0 +1,35 @@
+#pragma once
+#define Swim main
+#define Waddle int
+#define Eat return
+#define Bread 0
+#define Quack DuckLIB_Quack
+#define QUACK false
+#define HONK true
+#define Honk DuckLIB_Wait
+#define Fly void
+#include <iostream>
+#include <string>
+#include <locale>
+
+void DuckLIB_Quack(bool Option, std::string Text) {
+	std::locale Locale;
+	std::string Final_Text = "";
+	for (std::string::size_type Counter = 0; Counter < Text.length(); Counter++) {
+		if (Option) {
+			Final_Text = Final_Text + std::toupper(Text[Counter], Locale);
+		} else {
+			Final_Text = Final_Text + std::tolower(Text[Counter], Locale);
+		}
+	}
+	if (Option) {
+		std::cout << Final_Text + "!!!" << std::endl;
+	}
+	else {
+		std::cout << Final_Text << std::endl;
+	}
+}
+
+void DuckLIB_Wait() {
+	std::cin.get();
+}
