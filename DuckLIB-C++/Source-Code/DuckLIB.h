0d82ea2a112b07ac53091dc524946a0c2acdb4bc
Luna-Doggo <113269653+Luna-Doggo@users.noreply.github.com>
2023-09-21 11:21:54 -0700

    Quack. Quack quack?

--- DuckLIB-C++/Source-Code/DuckLIB.h
+++ DuckLIB-C++/Source-Code/DuckLIB.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <locale>
 #define Swim main
 #define Waddle int
 #define Eat return
@@ -8,3 +11,23 @@
 #define HONK true
 #define Honk DuckLIB_Wait
 #define Fly void
+
+void DuckLIB_Quack(bool Option, std::string Text) {
+	std::locale Locale;
+	std::string Final_Text = "";
+	for (std::string::size_type Counter = 0; Counter < Text.length(); Counter++) {
+		if (Option) {
+			Final_Text = Final_Text + std::toupper(Text[Counter], Locale);
+		} else {
+			Final_Text = Final_Text + std::tolower(Text[Counter], Locale);
+		}
+	}
+	if (Option) {
+		std::cout << Final_Text + "!!!" << std::endl;
+	} else {
+		std::cout << Final_Text << std::endl;
+	}
+}
+
+void DuckLIB_Wait() {
+  std::cin.get();
